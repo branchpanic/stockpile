d5f38c49614b5c47f5659148f68aac9c97baf38c
notjoe7F <computersunified@gmail.com>
2018-08-02 16:47:56 -0400

    Initial commit
    Totally didn't happen way too late.

---
+++ src/main/kotlin/notjoe/stockpile/block/BlockBarrel.kt
@@ -0,0 +1,88 @@
+package notjoe.stockpile.block
+
+import net.minecraft.block.Block
+import net.minecraft.block.BlockDirectional
+import net.minecraft.block.ITileEntityProvider
+import net.minecraft.block.material.Material
+import net.minecraft.block.state.IBlockState
+import net.minecraft.entity.EntityLivingBase
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.init.SoundEvents
+import net.minecraft.item.BlockItemUseContext
+import net.minecraft.state.StateContainer
+import net.minecraft.tileentity.TileEntity
+import net.minecraft.util.EnumFacing
+import net.minecraft.util.EnumHand
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.RayTraceFluidMode
+import net.minecraft.util.math.RayTraceResult
+import net.minecraft.util.math.Vec3d
+import net.minecraft.world.IBlockReader
+import net.minecraft.world.World
+import notjoe.stockpile.tile.TileBarrel
+import notjoe.stockpile.tile.inventory.OUTPUT_SLOT_INDEX
+
+class BlockBarrel :
+        BlockDirectional(Block.Builder.create(Material.WOOD).hardnessAndResistance(3f, 14f)), ITileEntityProvider {
+
+    init {
+        defaultState = blockState.baseState.withProperty(FACING, EnumFacing.NORTH)
+    }
+
+    override fun addPropertiesToBuilder(stateBuilder: StateContainer.Builder<Block, IBlockState>?) {
+        stateBuilder?.addProperties(FACING)
+    }
+
+    override fun hasTileEntity(): Boolean = true
+    override fun getTileEntity(p0: IBlockReader?): TileEntity? = TileBarrel()
+
+    override fun onLeftClick(state: IBlockState?, world: World?, pos: BlockPos?, player: EntityPlayer?) {
+        if (world == null || player == null || state == null) {
+            return
+        }
+
+        val tile = world.getTileEntity(pos) as TileBarrel
+        val amountToExtract = if (player.isSneaking) tile.inventoryStackLimit else 1
+        val extractedStack = tile.decrStackSize(OUTPUT_SLOT_INDEX, amountToExtract)
+
+        // TODO: Find out where the reach value is stored instead of hardcoding it.
+        val rayTraceResult = player.rayTraceFromEyes(4.0)
+
+        if (rayTraceResult != null && rayTraceResult.sideHit == getFacing(state) && !extractedStack.isEmpty) {
+            player.addItemStackToInventory(extractedStack)
+            player.playSound(SoundEvents.ENTITY_ITEM_PICKUP, 1f, 1f)
+        }
+    }
+
+    override fun onRightClick(state: IBlockState?, world: World?, pos: BlockPos?, player: EntityPlayer?,
+                              hand: EnumHand?, face: EnumFacing?, x: Float, y: Float, z: Float): Boolean {
+        if (world == null || player == null || world.isRemote) {
+            return true
+        }
+
+        val tile = world.getTileEntity(pos) as TileBarrel
+        tile.handleRightClick(player)
+
+        return true
+    }
+
+    override fun getBlockToPlaceOnUse(context: BlockItemUseContext?): IBlockState? {
+        if (context == null) {
+            return null
+        }
+
+        return defaultState.withProperty(FACING, context.func_196010_d())
+    }
+}
+
+fun getFacing(state: IBlockState): EnumFacing {
+    return state.getValue(BlockDirectional.FACING)
+}
+
+// Adapted from https://github.com/MinecraftForge/MinecraftForge/blob/2786cd279cd8feb5965060da27f141d7c8ccf1e5/src/main/java/net/minecraftforge/common/ForgeHooks.java#L1073
+fun EntityLivingBase.rayTraceFromEyes(distance: Double,
+                                      fluidMode: RayTraceFluidMode = RayTraceFluidMode.NEVER): RayTraceResult? {
+    val startPos = Vec3d(posX, posY + eyeHeight, posZ)
+    val endPos = startPos.add(lookVec.scale(distance))
+    return world.rayTraceBlocks(startPos, endPos, fluidMode)
+}
---
+++ src/main/kotlin/notjoe/stockpile/tile/inventory/MutableMassItemStorage.kt
@@ -0,0 +1,118 @@
+package notjoe.stockpile.tile.inventory
+
+import net.minecraft.item.ItemStack
+import kotlin.math.min
+
+const val OUTPUT_SLOT_INDEX = 0
+const val INPUT_SLOT_INDEX = 1
+
+/**
+ * Inventory implementation for storing a large amount of a single ItemStack.
+ *
+ * Contains two slots: one for output, and one for input (see constants INPUT_SLOT_INDEX and OUTPUT_SLOT_INDEX). The
+ * output slot always supplies as much of the contained item as possible. The input slot is empty until the amount
+ * reaches (maxStacks - 1) * stackLimit, in which it then contains the remaining stack.
+ *
+ * This implementation allows for the stack type to be mutated in-place.
+ */
+class MutableMassItemStorage(private var _stackType: ItemStack,
+                             val maxStacks: Int,
+                             var amount: Int = 0) :
+        AbstractSidedInventory("mass_item_storage") {
+
+    var stackType: ItemStack
+        get() = _stackType
+        set(value) {
+            _stackType = value
+            amount = 0
+        }
+
+    val availableSpace: Int get() = (maxStacks * inventoryStackLimit) - amount
+    val typeIsUndefined: Boolean get() = stackType.isEmpty
+
+    /**
+     * Inserts an ItemStack into this MutableMassItemStorage and returns the remainder.
+     */
+    fun insertStack(stack: ItemStack): ItemStack {
+        if (!isItemValidForSlot(INPUT_SLOT_INDEX, stack)) {
+            return stack
+        }
+
+        val insertableAmount = min(stack.count, availableSpace)
+        val remainderAmount = stack.count - insertableAmount
+        setInventorySlotContents(INPUT_SLOT_INDEX, stack)
+
+        return if (remainderAmount > 0) {
+            stack.withCount(remainderAmount)
+        } else {
+            ItemStack.EMPTY
+        }
+    }
+
+    override fun getStackInSlot(slotIndex: Int): ItemStack {
+        if (slotIndex == INPUT_SLOT_INDEX) {
+            val lastStackAmount = amount - ((maxStacks - 1) * inventoryStackLimit)
+            if (lastStackAmount > 0) {
+                return stackType.withCount(lastStackAmount)
+            }
+        }
+
+        if (slotIndex == OUTPUT_SLOT_INDEX) {
+            val availableStackAmount = min(amount, inventoryStackLimit)
+            if (availableStackAmount > 0) {
+                return stackType.withCount(availableStackAmount)
+            }
+        }
+
+        return ItemStack.EMPTY
+    }
+
+    override fun removeStackFromSlot(slotIndex: Int): ItemStack {
+        val workingStack = getStackInSlot(slotIndex)
+        if (workingStack.isEmpty) {
+            return ItemStack.EMPTY
+        }
+
+        amount -= workingStack.count
+        markDirty()
+
+        return workingStack
+    }
+
+    override fun clear() {
+        amount = 0
+    }
+
+    override fun getInventoryStackLimit(): Int = stackType.maxStackSize
+
+    override fun decrStackSize(slotIndex: Int, decrementAmount: Int): ItemStack {
+        val workingStack = getStackInSlot(slotIndex)
+        if (workingStack.isEmpty) {
+            return ItemStack.EMPTY
+        }
+
+        val decrementedAmount = min(workingStack.count, decrementAmount)
+        amount -= decrementedAmount
+        markDirty()
+
+        return workingStack.withCount(decrementedAmount)
+    }
+
+    override fun getSizeInventory(): Int = 2
+
+    override fun isEmpty(): Boolean = amount == 0
+
+    override fun isItemValidForSlot(slotIndex: Int, stack: ItemStack?): Boolean {
+        return !typeIsUndefined && stack != null && !stack.isEmpty &&
+                slotIndex == INPUT_SLOT_INDEX && stackType.isItemEqual(stack)
+    }
+
+    override fun setInventorySlotContents(slotIndex: Int, stack: ItemStack?) {
+        if (slotIndex != INPUT_SLOT_INDEX || stack == null || stack.isEmpty) {
+            return
+        }
+
+        amount += min(availableSpace, stack.count)
+        markDirty()
+    }
+}

d80fb0f3d6982ed5491b2bd3f07a4f3b020f0544
branchpanic <branchpanic@gmail.com>
2018-08-18 19:48:33 -0400

    Remove grated hopper from master

--- src/main/kotlin/notjoe/stockpile/block/StockpileBlocks.kt
+++ src/main/kotlin/notjoe/stockpile/block/StockpileBlocks.kt
@@ -13,18 +13,15 @@ class StockpileBlocks : BlockAdder, ItemAdder {
     companion object Definitions {
         val barrel = BlockBarrel()
         val trashCan = BlockTrashCan()
-        val gratedHopper = BlockGratedHopper()
     }
 
     override fun registerBlocks() {
         Block.register(ResourceLocation("stockpile", "barrel"), barrel)
         Block.register(ResourceLocation("stockpile", "trash_can"), trashCan)
-        Block.register(ResourceLocation("stockpile", "grated_hopper"), gratedHopper)
     }
 
     override fun registerItems() {
         Item.registerItemBlock(ItemBlock(barrel, Item.Builder().maxStackSize(1).group(ItemGroup.DECORATIONS)))
         Item.registerItemBlock(trashCan, ItemGroup.DECORATIONS)
-        Item.registerItemBlock(gratedHopper, ItemGroup.REDSTONE)
     }
 }

6afdc09035515d8f5f509373d32873fabebab53e
notjoe7F <computersunified@gmail.com>
2018-08-18 19:48:33 -0400

    Remove grated hopper from master

--- src/main/kotlin/notjoe/stockpile/block/BlockGratedHopper.kt
+++
@@ -1,7 +0,0 @@
-package notjoe.stockpile.block
-
-import net.minecraft.block.Block
-import net.minecraft.block.BlockHopper
-import net.minecraft.block.material.Material
-
-class BlockGratedHopper : BlockHopper(Block.Builder.create(Material.ROCK))

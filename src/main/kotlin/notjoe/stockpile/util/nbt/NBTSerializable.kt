04e974149f29f659fc1e6161ebbbc3427e07ba10
notjoe7F <computersunified@gmail.com>
2018-08-26 01:30:10 -0400

    Implement barrel locking to spec

--- src/main/kotlin/notjoe/stockpile/util/nbt/NBTSerializable.kt
+++ src/main/kotlin/notjoe/stockpile/util/nbt/NBTSerializable.kt
@@ -4,8 +4,10 @@ import net.minecraft.nbt.NBTTagCompound
 
 /**
  * Represents an object that can be saved to and loaded from an NBTTagCompound in-place.
+ *
+ * NOTE: In the future, it's very likely that this will be replaced by a library.
  */
-interface NBTSavable {
+interface NBTSerializable {
     fun saveToCompound(): NBTTagCompound
     fun loadFromCompound(compound: NBTTagCompound)
 }
--- src/test/kotlin/notjoe/stockpile/testutil/Matchers.kt
+++ src/test/kotlin/notjoe/stockpile/testutil/Matchers.kt
@@ -6,5 +6,9 @@ import net.minecraft.item.ItemStack
 import notjoe.stockpile.util.ext.isStackableWith
 
 fun matchStack(stack: ItemStack) = object : Matcher<ItemStack> {
-    override fun test(value: ItemStack): Result = Result(value.isStackableWith(stack), "given ItemStack $value is not stackable with $stack", "$value should not be stackable with $stack")
+    override fun test(value: ItemStack): Result = Result(
+        value.isStackableWith(stack),
+        "given ItemStack $value is not stackable with $stack",
+        "$value should not be stackable with $stack"
+    )
 }

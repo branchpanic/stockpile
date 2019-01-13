45f0fc2b98455b9fd917d944941581c39c5bc547
branchpanic <branchpanic@gmail.com>
2019-01-13 02:00:16 -0500

    Re-add comparator output to barrels and implement applyRotation and applyMirror (#15)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).  
 
+## [0.4.1] - 2019-01-12
+
+### Added
+ - Comparator output for barrels, since it wasn't implemented in the previous version
+ 
+### Fixed
+ - Version being reported as 1.0.0 
+
+
 ## [0.4.0] - 2019-01-12
 
 ### Added
--- src/main/scala/notjoe/stockpile/block/FacingDirection.scala
+++ src/main/scala/notjoe/stockpile/block/FacingDirection.scala
@@ -4,6 +4,7 @@ import net.minecraft.block.{Block, BlockState}
 import net.minecraft.item.ItemPlacementContext
 import net.minecraft.state.StateFactory
 import net.minecraft.state.property.Properties
+import net.minecraft.util.{Mirror, Rotation}
 
 trait FacingDirection extends Block {
   abstract override def appendProperties(builder: StateFactory.Builder[Block, BlockState]): Unit = {
@@ -13,5 +14,13 @@ trait FacingDirection extends Block {
 
   override def getPlacementState(context: ItemPlacementContext): BlockState = {
     super.getPlacementState(context).`with`(Properties.FACING, context.getPlayerFacing.getOpposite)
+  }
+
+  override def applyRotation(state: BlockState, rotation: Rotation): BlockState = {
+    state.`with`(Properties.FACING, rotation.method_10503(state.get(Properties.FACING)))
+  }
+
+  override def applyMirror(state: BlockState, mirror: Mirror): BlockState = {
+    state.`with`(Properties.FACING, mirror.apply(state.get(Properties.FACING)))
   }
 }

4722c878e5ffc7e90506aa806bad2c026cd231d4
branchpanic <branchpanic@gmail.com>
2019-04-05 22:54:50 -0400

    Implement flexible mass ItemStack storage

---
+++ src/test/kotlin/me/branchpanic/mods/stockpile/api/storage/testbootstrap.kt
@@ -0,0 +1,16 @@
+package me.branchpanic.mods.stockpile.api.storage
+
+import net.minecraft.item.Item
+import net.minecraft.item.Items
+import org.powermock.reflect.Whitebox
+
+object TestItems {
+    init {
+        Whitebox.setInternalState(Items::class.java, "AIR", Item(Item.Settings()))
+    }
+
+    val StandardItemA = Item(Item.Settings())
+    val StandardItemB = Item(Item.Settings())
+
+    val QuarterStackingItem = Item(Item.Settings().stackSize(16))
+}

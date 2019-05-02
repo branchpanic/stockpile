5c0667e332ed5b383b94061394330fe69816a9c4
notjoe7F <computersunified@gmail.com>
2019-05-01 21:25:27 -0400

    Modify Barrel left-click logic

---
+++ src/main/kotlin/me/branchpanic/mods/stockpile/content/block/AttackableBlockCallback.kt
@@ -0,0 +1,33 @@
+package me.branchpanic.mods.stockpile.content.block
+
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.util.ActionResult
+import net.minecraft.util.Hand
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
+import net.minecraft.world.World
+
+object AttackableBlockCallback : AttackBlockCallback {
+    override fun interact(
+        player: PlayerEntity?,
+        world: World?,
+        hand: Hand?,
+        pos: BlockPos?,
+        direction: Direction?
+    ): ActionResult {
+        if (player == null ||
+            world == null ||
+            hand == null ||
+            pos == null ||
+            direction == null ||
+            player.isSpectator ||
+            player.isCreative
+        ) {
+            return ActionResult.PASS
+        }
+
+        val block = (world.getBlockState(pos).block as? AttackableBlock) ?: return ActionResult.PASS
+        return block.onBlockAttacked(player, world, hand, pos, direction)
+    }
+}
